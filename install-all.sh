47bfa6b4b1cb8d3f4d53186bc326d916f061234a
IbnSaad2910 <alif2910@gmail.com>
2025-11-15 03:00:20 +0800

    fix: remove install scripts

--- install-all.sh
+++ install-all.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
 # Install all packages in order
-. ./install-android-studio.sh
 . ./install-bitwarden.sh
 . ./install-brave.sh
 . ./install-bruno.sh
 . ./install-dbeaver.sh
 . ./install-freedownloadmanager.sh
 . ./install-kdeconnect.sh
-. ./install-mpv.sh
 . ./install-qbittorrent.sh
 . ./install-steam.sh
 . ./install-stow.sh
